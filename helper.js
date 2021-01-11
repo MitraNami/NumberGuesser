37b87d5b551a457ecf3aa5f67dade37c05e7f480
Mitra Nami <a.mxnami@gmail.com>
2021-01-11 03:21:42 -0500

    Move the helper functions to helper module

--- helper.js
+++ helper.js
@@ -1,3 +1,20 @@
+// returns true if the entered number is valid :not left empty, not smaller than min,
+// not greater than max; otherwise false
+const checkValidity = function(val, min, max) {
+  if (val < min || val > max || isNaN(val)) {
+    setMessage(`Enter a number between ${min} and ${max}`, 'red');
+    return false;
+  }
+  return true;
+};
+
+
+// Set message in the given color
+const setMessage = function(msg, color) {
+  const messageEl = document.querySelector('.message');
+  messageEl.textContent = msg;
+  messageEl.style.color = color;
+};
 
 
 // This function should be called when the game is finished
